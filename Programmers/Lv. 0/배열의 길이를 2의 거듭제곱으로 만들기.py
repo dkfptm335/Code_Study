1295a0ebac005495e0cd5147aa1a4c2263b566c1
Gloomy <dkfptm335@naver.com>
2024-02-01 18:39:08 +0900

    2024-02-01 Programmers

---
+++ Programmers/Lv. 0/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -0,0 +1,16 @@
+# 문제 설명
+# 정수 배열 arr이 매개변수로 주어집니다. arr의 길이가 2의 정수 거듭제곱이 되도록 arr 뒤에 정수 0을 추가하려고 합니다.
+# arr에 최소한의 개수로 0을 추가한 배열을 return 하는 solution 함수를 작성해 주세요.
+
+def solution(arr):
+    answer = []
+    # 2의 지수 구하기
+    idx = 0
+    
+    while 2**idx < len(arr):
+        idx += 1
+    
+    for i in range(2**idx - len(arr)):
+        answer.append(0)
+
+    return answer
