971ecf6735a6311b352d8fcc51a5a4bf64004d1e
gloomy <dkfptm335@naver.com>
2024-02-06 10:05:16 +0900

    2024-02-06 Programmers

---
+++ Programmers/Lv. 1/자연수 뒤집어 배열로 만들기.py
@@ -0,0 +1,4 @@
+def solution(n):
+    numbers = list(map(int, str(n)))
+    numbers.reverse()
+    return numbers
---
+++ Programmers/Lv. 1/정수 제곱근 판별.py
@@ -0,0 +1,6 @@
+def solution(n):
+    if n**0.5 == int(n**0.5):
+        return (int(n**0.5)+1)**2
+    else:
+        return -1
+    
