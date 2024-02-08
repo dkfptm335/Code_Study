1084a833af378cc1d80b4f4d7ed9c6e5c7871228
gloomy <dkfptm335@naver.com>
2024-02-08 17:23:20 +0900

    2024-02-08 Programmers

---
+++ Programmers/Lv. 1/2018 KAKAO BLIND RECRUITMENT/[1차] 비밀지도.py
@@ -0,0 +1,10 @@
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i in range(len(arr1)):
+        answer.append(bin(arr1[i] | arr2[i])[2:].zfill(n).replace('1', '#').replace('0', ' '))
+        
+    for i in range(len(answer)):
+        print(answer[i])
+    
+    return answer
