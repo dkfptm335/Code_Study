06e1d436536c55c38b1ff23be23ab6f61e2590fb
Gloomy <dkfptm335@naver.com>
2024-02-14 17:49:44 +0900

    2024-02-14 Programmers

--- Programmers/Lv. 1/연습문제/햄버거 만들기.py
+++ Programmers/Lv. 1/연습문제/햄버거 만들기.py
@@ -1,9 +1,17 @@
-def solution(ingredient):
-    s = []
-    cnt = 0
-    for i in ingredient:
-        s.append(i)
-        if s[-4:] == [1, 2, 3, 1]:
-            cnt += 1
-            del s[-4:]
-    return cnt
+def solution(ingredients):
+    # 햄버거를 만들 수 있는 패턴
+    pattern = [1, 2, 3, 1]
+    # 포장하는 햄버거의 개수
+    hamburgers = 0
+    
+    stack = []
+    
+    for ingredient in ingredients:
+        stack.append(ingredient)
+        if len(stack) >= 4:
+            if stack[-4:] == pattern:
+                for _ in range(4):
+                    stack.pop()
+                hamburgers += 1
+                
+    return hamburgers
