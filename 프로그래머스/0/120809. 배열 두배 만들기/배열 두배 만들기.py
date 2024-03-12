fbe50e0e9504ab5585788262689086fe3fd0daa2
Gloomy <dkfptm335@naver.com>
2024-03-12 13:05:00 +0900

    change folder name

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++
@@ -1,7 +0,0 @@
-def solution(numbers):
-    answer = []
-    
-    for i in range(len(numbers)):
-        answer.append(2 * numbers[i])
-    
-    return answer
--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Solution {
-    public int[] solution(int[] num_list) {
-        Array.Reverse(num_list);
-        int[] answer = num_list;
-        return answer;
-    }
-}
