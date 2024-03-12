21ac73a7a328ed9d55a7a99f4816766ff1c64531
Sungjun Jung <dkfptm335@naver.com>
2024-03-12 13:00:14 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.14 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        double answer = 0;
+        
+        answer = (double)num1 / (double)num2 * 1000;
+        
+        return (int)answer;
+    }
+}
