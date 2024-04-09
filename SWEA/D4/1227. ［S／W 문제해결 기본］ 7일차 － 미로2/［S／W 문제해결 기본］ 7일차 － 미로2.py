4e711ec4013ce011d8c5a1a573c1a35c583f3215
Sungjun Jung <dkfptm335@naver.com>
2024-04-09 12:14:41 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 293 ms, Memory: 52,324 KB -BaekjoonHub

---
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+for tc in range(1, 11):
+    testNum = int(input())
+    maze = [list(map(int, input())) for _ in range(100)]
+    isPossible = False
+    visited = [[False] * 100 for _ in range(100)]
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = deque()
+
+    for i in range(100):
+        for j in range(100):
+            if maze[i][j] == 2:
+                queue.append((i, j))
+                break
+
+    while queue:
+        x, y = queue.popleft()
+        visited[x][y] = True
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0 <= nx < 100 and 0 <= ny < 100:
+                if maze[nx][ny] == 0 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+                if maze[nx][ny] == 3 and not visited[nx][ny]:
+                    isPossible = True
+                    break
+
+    print(f"#{tc} {1 if isPossible else 0}")
