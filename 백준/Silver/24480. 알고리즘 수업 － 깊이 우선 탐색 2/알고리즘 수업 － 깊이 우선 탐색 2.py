f4207e9d7358c72acafc63c4203e02d7ddb2732c
Sungjun Jung <dkfptm335@naver.com>
2024-04-10 00:45:25 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 768 ms, Memory: 76108 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
+
+N, M, R = map(int, sys.stdin.readline().strip().split())
+graph = {i: [] for i in range(1, N + 1)}
+visited = [0] * (N + 1)
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().strip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for key in graph.keys():
+    graph[key].sort(reverse=True)
+
+
+count = 0
+
+
+def dfs(v):
+    global count
+    count += 1
+    visited[v] = count
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(i)
+
+
+dfs(R)
+
+for i in range(1, N + 1):
+    print(visited[i])
