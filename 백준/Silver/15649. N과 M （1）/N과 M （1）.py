eb27e84b847f8faa3c98d8d2acf0069aea07a01c
Sungjun Jung <dkfptm335@naver.com>
2024-04-30 16:42:33 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().strip().split())
+visitLog = []
+
+
+def dfs():
+    if len(visitLog) == M:
+        print(' '.join(map(str, visitLog)))
+        return
+
+    for i in range(1, N + 1):
+        if i not in visitLog:
+            visitLog.append(i)
+            dfs()
+            visitLog.pop()
+
+
+dfs()
