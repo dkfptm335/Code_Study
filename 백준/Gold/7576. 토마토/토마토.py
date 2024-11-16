d445a8cd00bfd1434b1b0e40f50dfd2478e4f9ca
Gloomy <dkfptm335@naver.com>
2024-11-17 03:37:31 +0900

    [Gold V] Title: 토마토, Time: 1924 ms, Memory: 98612 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,31 +1,35 @@
 import sys
 from collections import deque
 
-
 M, N = map(int, sys.stdin.readline().strip().split())
-field = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+box = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 queue = deque()
+
+# 탐색 시작 지점 큐에 추가
 for i in range(N):
     for j in range(M):
-        if field[i][j] == 1:
+        if box[i][j] == 1:
             queue.append((i, j))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
 while queue:
     x, y = queue.popleft()
+
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < N and 0 <= ny < M:
-            if field[nx][ny] == 0:
-                field[nx][ny] = field[x][y] + 1
+            if box[nx][ny] == 0:
                 queue.append((nx, ny))
+                box[nx][ny] = box[x][y] + 1
 
 day = 0
 
-for row in field:
+# 모든 토마토가 익었는지 확인
+for row in box:
     for tomato in row:
         if tomato == 0:
             print(-1)
